0f73afc012583bc6f3b8893f8200f6e403088abd
Rudenman <rudenko.do@edu.spbstu.ru>
2023-08-05 07:15:48 +0300

    added bubble sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,19 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Random;
 
 public class Main {
 
-    public static void quickSort(int[] arr, int startIndex, int endIndex) {
-        if (startIndex < endIndex) {
-            int divideIndex = partition(arr, startIndex, endIndex);
-            quickSort(arr, startIndex, divideIndex - 1);
-            quickSort(arr, divideIndex, endIndex);
-        }
-    }
-
-    private static int partition(int[] arr, int startIndex, int endIndex) {
-        int leftIndex = startIndex, rightIndex = endIndex;
-        int pivot = arr[new Random().nextInt(endIndex - startIndex) + startIndex];
-        while (leftIndex <= rightIndex) {
-            while (arr[leftIndex] < pivot) {
-                leftIndex++;
-            }
-            while (arr[rightIndex] > pivot) {
-                rightIndex--;
-            }
-            if (leftIndex <= rightIndex) {
-                swap(arr, leftIndex, rightIndex);
-                leftIndex++;
-                rightIndex--;
+    public static void bubbleSort(int[] arr) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                    isSorted = false;
+                }
             }
         }
-        return leftIndex;
     }
 
     private static void swap(int[] arr, int leftIndex, int rightIndex) {
@@ -37,5 +21,4 @@ public class Main {
         arr[leftIndex] = arr[rightIndex];
         arr[rightIndex] = tmp;
     }
-
 }
