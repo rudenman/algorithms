13ca598b0f147afa98bd0029ea693e0edd4fff88
Rudenman <rudenko.do@edu.spbstu.ru>
2023-08-05 08:12:56 +0300

    added merge sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,43 @@ package org.example;
 
 public class Main {
 
-    public static void bubbleSort(int[] arr) {
-        boolean isSorted = false;
-        while (!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    swap(arr, i, i + 1);
-                    isSorted = false;
-                }
+    public static int[] mergeSort(int[] arr) {
+        int[] tmp;
+        int[] currentSrc = arr;
+        int[] currentDest = new int[arr.length];
+
+        int size = 1;
+        while (size < arr.length) {
+            for (int i = 0; i < arr.length; i += 2 * size) {
+                merge(currentSrc, i, i + size, currentDest, i, size);
+            }
+            tmp = currentSrc;
+            currentSrc = currentDest;
+            currentDest = tmp;
+
+            size *= 2;
+        }
+        return currentSrc;
+    }
+
+    private static void merge(int[] src, int src1Start, int src2Start, int[] dest,
+                              int destStart, int size) {
+        int index1 = src1Start, index2 = src2Start;
+
+        int src1End = Math.min(src1Start + size, src.length);
+        int src2End = Math.min(src2Start + size, src.length);
+
+        int iterationCount = src1End - src1Start + src2End - src2Start;
+
+        for (int i = destStart; i < destStart + iterationCount; i++) {
+            if (index1 < src1End && (index2 >= src2End || src[index1] < src[index2])) {
+                dest[i] = src[index1];
+                index1++;
+            } else {
+                dest[i] = src[index2];
+                index2++;
             }
         }
     }
 
-    private static void swap(int[] arr, int leftIndex, int rightIndex) {
-        int tmp = arr[leftIndex];
-        arr[leftIndex] = arr[rightIndex];
-        arr[rightIndex] = tmp;
-    }
 }
