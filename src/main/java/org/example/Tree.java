2916bf2feaec9237176cc080f5054c8bde6d852e
Rudenman <rudenko.do@edu.spbstu.ru>
2023-08-06 14:15:39 +0300

    final fix

--- src/main/java/org/example/Tree.java
+++ src/main/java/org/example/Tree.java
@@ -1,2 +1,53 @@
-package org.example;public class Tree {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Tree {
+
+    private final int value;
+
+    private List<Tree> relatedNotesList = new ArrayList<>();
+
+    public Tree(int value, Tree[] relatedNotesArray) {
+        this.value = value;
+        relatedNotesList = Arrays.asList(relatedNotesArray);
+    }
+
+    public Tree(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public List<Tree> getRelatedNotesList() {
+        return relatedNotesList;
+    }
+
+    public int recursiveSum() {
+        int sum = value;
+
+        for (Tree vertex : relatedNotesList) {
+            sum += vertex.recursiveSum();
+        }
+        return sum;
+    }
+
+    public int deepSum() {
+        int sum = 0;
+        Stack<Tree> stack = new Stack<>();
+        stack.push(this);
+        while (!stack.isEmpty()) {
+            Tree commonVertex = stack.pop();
+            sum += commonVertex.getValue();
+            for (Tree vertex : commonVertex.getRelatedNotesList()) {
+                stack.push(vertex);
+            }
+        }
+        return sum;
+    }
 }
--- src/test/java/org/example/TreeTest.java
+++ src/test/java/org/example/TreeTest.java
@@ -1,2 +1,66 @@
-package org.example;public class TreeTest {
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TreeTest {
+
+    private final Tree tree =
+            new Tree(10,
+                    new Tree[]{new Tree(12),
+                            new Tree(13),
+                            new Tree(14)});
+
+    private final Tree simpleTree =
+            new Tree(10, new Tree[]{
+                    new Tree(11, new Tree[]{
+                            new Tree(4)}),
+                    new Tree(5)
+            });
+
+    private final Tree hardTree =
+            new Tree(3, new Tree[]{
+                    new Tree(1, new Tree[]{
+                            new Tree(5),
+                            new Tree(6, new Tree[]{
+                                    new Tree(50),
+                                    new Tree(30, new Tree[]{
+                                            new Tree(50)
+                                    })
+                            })
+                    }),
+                    new Tree(4, new Tree[]{
+                            new Tree(18, new Tree[]{
+                                    new Tree(30),
+                                    new Tree(45)
+                            }),
+                            new Tree(12, new Tree[]{
+                                    new Tree(20)
+                            }),
+                            new Tree(111, new Tree[]{
+                                    new Tree(3),
+                                    new Tree(50),
+                                    new Tree(40),
+                                    new Tree(60)
+                            })
+                    })
+            });
+
+
+    @Test
+    public void testRecursiveSum() {
+        assertEquals(49, tree.recursiveSum());
+        assertEquals(30, simpleTree.recursiveSum());
+        assertEquals(538, hardTree.recursiveSum());
+    }
+
+    @Test
+    public void testDeepSum() {
+        assertEquals(49, tree.deepSum());
+        assertEquals(30, simpleTree.deepSum());
+        assertEquals(538, hardTree.deepSum());
+    }
 }
